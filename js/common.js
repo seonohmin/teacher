64aa766554b78dd7b84db6388c4dd886b81ec54e
seonohmin <seon.o.min@gmail.com>
2023-04-03 09:39:01 +0900

    main

--- js/common.js
+++ js/common.js
@@ -1,123 +1,121 @@
-
-(function () {
-	var thisBrowser = navigator.userAgent.toLowerCase();
-	var html = $("html");
-
-	if(
-		thisBrowser.indexOf("ipod") > -1 || 
-		thisBrowser.indexOf("iphone") > -1 || 
-		thisBrowser.indexOf("ipad") > -1 ||
-		thisBrowser.indexOf("android") > -1 || 
-		thisBrowser.indexOf("windows phone") > -1
-	) {
-		html.addClass("mob_mode");
-	}
-	else{
-		html.addClass("pc");
-	}
-})();
-
-
-/* 디바이스설정 - OS, Version, Browser */
-function setDevices(){
-	var cls = 'dv_';
-	var browserDevice = function(){ return browser.mobile == true ? 'mobile' : 'pc' }
-	var clsBrowser = ''
-		+ cls + browser.name
-		+ ' ' + cls + browser.name + browser.version
-		+ ' ' + cls + browser.os
-		+ ' ' + cls + browser.os + Math.floor(browser.osVersion)
-		+ ' ' + cls + browserDevice();
-	$body.addClass(clsBrowser);
-}
-
-// touch device check
-const isTouchDevice = () => 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-
 /* ========================================================================
  * gnb hover
  * ======================================================================== */
 
 $(function () {
-    var $header = $('#header');
+  function detectDevice() {
+    const $html = $("html");
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    const isTablet = /Tablet|iPad/i.test(navigator.userAgent);
+    const isDesktop = !isMobile && !isTablet;
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints;
 
-    initialize();
-    $(window).on('resize', initialize);
-    
-    function initialize() {
-      if ($(window).width() >= 980) {
-        $('#gnb .gnb_menu').on('mouseover focusin', headerHoverOn);
-        $('#gnb .gnb_menu').on('mouseleave focusout', headerHoverOff);
-      } else {
-        $('#gnb .gnb_menu').off('mouseover focusin', headerHoverOn);
-        $('#gnb .gnb_menu').off('mouseleave focusout', headerHoverOff);
-        $header.removeClass('hover');
-      }
+    if (isMobile || isTouchDevice) {
+      $html.addClass("mobile");
+    } else if (isTablet) {
+      $html.addClass("tablet");
+    } else if (isDesktop) {
+      $html.addClass("pc");
     }
-    
-    function headerHoverOn() { $header.addClass('hover'); }  
-    function headerHoverOff() { $header.removeClass('hover'); }
+  } 
 
-    $('.toggle_btn').click(function() {
-      $('.sitemap').toggleClass('open');
-    });
-    $('.sitemap_dim').click(function() {
-      $('.sitemap').toggleClass('open');
-    });
+  function initGnb() {
+      const $header = $("#header");
+      const $gnbMenu = $("#gnb .gnb_menu");
 
-    $('.sitemap .bt_more').click(function(){
-      if ($(this).hasClass('on')) {
-        $(this).removeClass('on');
-        $(this).parent().removeClass('on');
-        $(this).next('.sm').slideUp();
-      } else {
-        $('.sitemap .mm').removeClass('on');
-        $('.sitemap .sm').slideUp();
-        $(this).addClass('on');
-        $(this).parent().addClass('on').siblings().removeClass('on');
-        $(this).next('.sm').slideDown();
+      function headerHoverOn() {
+          $header.addClass("hover");
       }
-    });
 
-    /* 패밀리 사이트 */
-    var $select = $('#institution_select');
-    var $label = $('label[for="institution_select"]');
-
-    $select.change(function() {
-      var selectText = $select.children('option:selected').text();
-      $label.text(selectText);
-    });
-
-    $select.change(function() {
-      var selectedValue = $select.val();
-      if(selectedValue != '') {
-        window.open(selectedValue, '_blank');
+      function headerHoverOff() {
+          $header.removeClass("hover");
       }
-    });
 
-    /* 맨위로 가기 */
-    $(window).scroll(function() {
-      const $btnTop = $('.btn_top');
-      if ($(this).scrollTop() > 500) {
-        $btnTop.addClass('show');
-      } else {
-        $btnTop.removeClass('show');
+      function gnbResizeHandler() {
+          if ($(window).width() >= 980) {
+              $gnbMenu.on("mouseover focusin", headerHoverOn);
+              $gnbMenu.on("mouseleave focusout", headerHoverOff);
+          } else {
+              $gnbMenu.off("mouseover focusin", headerHoverOn);
+              $gnbMenu.off("mouseleave focusout", headerHoverOff);
+              $header.removeClass("hover");
+          }
       }
-    });
-    
-    $('.btn_top').click(function() {
-      $('html, body').animate({scrollTop: 0}, 680);
-    });
 
+      gnbResizeHandler();
+      $(window).on("resize", gnbResizeHandler);
+  }
+
+  function hideTopBanner() {
+      $(".btn_close").on("click", function () {
+          $("#top_banner").hide();
+          $("#wrap").addClass("ban_close");
+      });
+  }
+
+  function initSitemap() {
+      $(".toggle_btn, .sitemap_dim").click(function () {
+          $(".sitemap").toggleClass("open");
+      });
+
+      $(".sitemap .bt_more").click(function () {
+          const $this = $(this);
+          if ($this.hasClass("on")) {
+              $this.removeClass("on");
+              $this.parent().removeClass("on");
+              $this.next(".sm").slideUp();
+          } else {
+              $(".sitemap .mm").removeClass("on");
+              $(".sitemap .sm").slideUp();
+              $this.addClass("on");
+              $this.parent().addClass("on").siblings().removeClass("on");
+              $this.next(".sm").slideDown();
+          }
+      });
+  }
+
+  function initInstitutionSelect() {
+      const $select = $("#institution_select");
+      const $label = $('label[for="institution_select"]');
+
+      $select.change(function () {
+          const selectText = $select.children("option:selected").text();
+          $label.text(selectText);
+      });
+
+      $select.change(function () {
+          const selectedValue = $select.val();
+          if (selectedValue !== "") {
+              window.open(selectedValue, "_blank");
+          }
+      });
+  }
+
+  function initBackToTop() {
+      const $btnTop = $(".btn_top");
+
+      $(window).scroll(function () {
+          if ($(this).scrollTop() > 500) {
+              $btnTop.addClass("show");
+          } else {
+              $btnTop.removeClass("show");
+          }
+      });
+
+      $btnTop.click(function () {
+          $("html, body").animate({ scrollTop: 0 }, 680);
+      });
+  }
+
+  $(function () {
+    detectDevice();
+    initGnb();
+    hideTopBanner();
+    initSitemap();
+    initInstitutionSelect();
+    initBackToTop();
+  });
 });
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  ui.init();
-  ui.tabs();
-  ui.dropDownTab();
-  ui.accordion();
-  ui.allMenu();
-  ui.dropdown();
-});
